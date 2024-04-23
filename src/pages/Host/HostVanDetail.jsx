f30dbb7a5805d2ac2fca9915cae0aabb2f12b238
AlanBrad <ambradnum@hotmail.com>
2024-04-23 17:14:34 +0100

    Before Protected Routes Challenge 2

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { useParams, Link, NavLink, Outlet } from 'react-router-dom';
+import {
+  useParams,
+  Link,
+  NavLink,
+  Outlet,
+  useLoaderData,
+} from 'react-router-dom';
+import { getHostVans } from '../../api';
+
+export function loader({ params }) {
+  return getHostVans(params.id);
+}
 
 export default function HostVanDetail() {
-  const { id } = useParams();
-  const [currentVan, setCurrentVan] = React.useState(null);
+  const currentVan = useLoaderData();
 
   const activeStyles = {
     fontWeight: 'bold',
     textDecoration: 'underline',
     color: '#161616',
   };
-
-  React.useEffect(() => {
-    fetch(`/api/host/vans/${id}`)
-      .then((res) => res.json())
-      .then((data) => setCurrentVan(data.vans));
-  }, []);
-  // }, [id]);
-
-  if (!currentVan) {
-    return <h1>Loading...</h1>;
-  }
 
   return (
     <section>
@@ -31,7 +30,9 @@ export default function HostVanDetail() {
         <div className='host-van-detail'>
           <img src={currentVan.imageUrl} />
           <div className='host-van-detail-info-text'>
-            <i className={`van-type ${currentVan.type}`}>{currentVan.type}</i>
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
             <h3>{currentVan.name}</h3>
             <h4>${currentVan.price}/day</h4>
           </div>{' '}
--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import { getHostVans } from '../../api';
+
+export function loader() {
+  return getHostVans();
+}
 
 export default function HostVans() {
-  const [vans, SetVans] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch('/api/host/vans')
-      .then((res) => res.json())
-      .then((data) => SetVans(data.vans));
-  }, []);
+  const vans = useLoaderData();
 
   const hostVanElements = vans.map((van) => (
     <Link
@@ -31,11 +30,7 @@ export default function HostVans() {
     <section>
       <h1 className='host-vans-title'>Your listed vans</h1>
       <div className='host-vans-list'>
-        {vans.length > 0 ? (
-          <section>{hostVanElements}</section>
-        ) : (
-          <h2>Loading...</h2>
-        )}
+        <section>{hostVanElements}</section>
       </div>
     </section>
   );
