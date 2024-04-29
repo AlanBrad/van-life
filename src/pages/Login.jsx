521ce4063255be79bbe53cf61706869facaa1293
AlanBrad <ambradnum@hotmail.com>
2024-04-29 18:55:43 +0100

    After useNavigation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { useLoaderData, useNavigate, redirect, Form } from 'react-router-dom';
+import {
+  useActionData,
+  useLoaderData,
+  // useNavigate,
+  useNavigation,
+  redirect,
+  Form,
+} from 'react-router-dom';
 import { loginUser } from '../api';
 
 export function loader({ request }) {
@@ -10,34 +17,38 @@ export async function action({ request }) {
   const formData = await request.formData();
   const email = formData.get('email');
   const password = formData.get('password');
-  const data = await loginUser({ email, password });
-  // console.log('here');
-  localStorage.setItem('loggedin', true);
-  // console.log(data);
-  const response = redirect('/host');
-  throw Object.defineProperty(response, 'body', { value: true });
-  return null;
+  // const data = await loginUser({ email, password });
+  // localStorage.setItem('loggedin', true);
+  // const response = redirect('/host');
+  // throw Object.defineProperty(response, 'body', { value: true });
+  // return null;
+  try {
+    const data = await loginUser({ email, password });
+    localStorage.setItem('loggedin', true);
+    const response = redirect('/host');
+    return Object.defineProperty(response, 'body', { value: true });
+  } catch (err) {
+    return err.message;
+  }
 }
 
 export default function Login() {
-  // const [loginFormData, setLoginFormData] = React.useState({
-  //   email: '',
-  //   password: '',
-  // });
-  const [status, setStatus] = React.useState('idle');
-  const [error, setError] = React.useState(null);
+  // const [status, setStatus] = React.useState('idle');
+  // const [error, setError] = React.useState(null);
+  const errorMessage = useActionData();
   const message = useLoaderData();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const navigation = useNavigation();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setStatus('submitting');
-    setError(null);
-    const user = loginUser(loginFormData)
-      .then((data) => navigate('/host', { replace: true }))
-      .catch((err) => setError(err))
-      .finally(() => setStatus('idle'));
-  }
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   setStatus('submitting');
+  //   // setError(null);
+  //   const user = loginUser(loginFormData)
+  //     .then((data) => navigate('/host', { replace: true }))
+  //     // .catch((err) => setError(err))
+  //     .finally(() => setStatus('idle'));
+  // }
 
   // function handleChange(e) {
   //   const { name, value } = e.target;
@@ -52,26 +63,14 @@ export default function Login() {
   return (
     <div className='login-container'>
       <h1>Sign in to your account</h1>
-      {error && <h3 className='red'>{error.message}</h3>}
+      {/* {error && <h3 className='red'>{error.message}</h3>} */}
       {message && <h3 className='red'>{message}</h3>}
-      {/* <Form onSubmit={handleSubmit} className='login-form'> */}
+      {errorMessage && <h3 className='red'>{errorMessage}</h3>}
       <Form method='post' className='login-form' replace>
-        <input
-          name='email'
-          // onChange={handleChange}
-          type='email'
-          placeholder='Email address'
-          // value={loginFormData.email}
-        />
-        <input
-          name='password'
-          // onChange={handleChange}
-          type='password'
-          placeholder='Password'
-          // value={loginFormData.password}
-        />
-        <button disabled={status === 'submitting'}>
-          {status === 'submitting' ? 'Logging in...' : 'Log in'}
+        <input name='email' type='email' placeholder='Email address' />
+        <input name='password' type='password' placeholder='Password' />
+        <button disabled={navigation.state === 'submitting'}>
+          {navigation.state === 'submitting' ? 'Logging in...' : 'Log in'}
         </button>
       </Form>
     </div>
